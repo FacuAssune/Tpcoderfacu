f1312f720711aa452e571e23497f3dd96e34bc52
FacuAssune <fassune@gmail.com>
2022-03-23 22:29:36 -0300

    Trabajo tp 5

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom' 
 import Navbar from './components/Nabvar'
+import ShoppingCart from './components/ShoppingCart';
+import ProductItem from './components/ProductItem';
+import { shoppingReducer } from './reducers/shoppingReducer';
+
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar/>
       </Router>
+      <ShoppingCart/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
